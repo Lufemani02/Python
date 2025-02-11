2a23f21d043dd0d2f17725f449283990909b9234
Luis Martinez <114120818+Lufemani02@users.noreply.github.com>
2025-02-11 16:28:03 -0500

    Delete Bottle_of_thoughts.py

--- Bottle_of_thoughts.py
+++
@@ -1,7 +0,0 @@
-from helper_functions import print_llm_response, get_llm_response
-
-print("¿ Sientes que tus pensamientos y tus emociones toman el control de tu vida ? ")
-print(" No te preocupes, a muchos nos ha pasado lo mismo y hoy te presento al Bottle_of_thoughts")
-
-thought1 = input(" ¿Qué pensamiento o emoción te impide avanzar, o ser tu mismo?")
-print_llm_response = f" Escribe una frase alentadora y una posible solucón con respecto a {thought1}"
